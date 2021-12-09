7cce643450ec930c77e5f20c3fc772e09f6089ac
David Hong <phazex1@gmail.com>
2021-12-09 17:43:10 -0500

    Added voice activation functionality

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -4,19 +4,23 @@ import { Doughnut } from 'react-chartjs-2';
 import useStyles from './detailsStyles';
 import useTransactions from '../../useTransactions';
 
-import {Chart, ArcElement} from 'chart.js'
+import {Chart, ArcElement } from 'chart.js'
 Chart.register(ArcElement);
+
+
+
 
 const Details = ({ title }) => {
     const classes = useStyles();
     const { total, chartData } = useTransactions(title);
 
+    console.log(chartData.labels)
     return (
         <Card className={title === 'Income' ? classes.income : classes.expense}>
             <CardHeader title={title} />
             <CardContent>
                 <Typography variant='h5'>${total}</Typography>
-                <Doughnut data={chartData} />
+                <Doughnut data={chartData}/>
             </CardContent>
         </Card>
     )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from './context/context'
+import { SpeechProvider } from '@speechly/react-client';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
+    <SpeechProvider appId='40087ee6-ce80-4917-990c-3a85a54bc2e0' language='en-US'>
     <Provider>
         <App />
-    </Provider>,
+    </Provider>
+    </SpeechProvider>,
     document.getElementById('root')
 );
